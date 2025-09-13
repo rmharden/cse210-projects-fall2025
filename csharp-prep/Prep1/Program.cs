b4deb4ba35e338546252444f9100134f0226504c
rmharden <har22079@byui.edu>
2025-09-13 16:19:16 -0400

    Added a read line and a variable to store the string of the first name from the user as well as added a writeline to ask the user what their last name is. I added a string variable to store the user's answer in last.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+
+        
     }
 }
