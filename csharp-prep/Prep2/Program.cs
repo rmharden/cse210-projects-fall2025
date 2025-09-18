86fc68b7bc67db1c5476bef0fc7dfd6b21785344
rmharden <har22079@byui.edu>
2025-09-18 16:44:58 -0400

    Completed step 3 and added a displayed message of their grade.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -15,7 +15,7 @@ class Program
     static void Main(string[] args)
     {
         // 1. Asks the user for their grade percentage and stores their input into a string varible. Then the string variable is converted into an interger by using the int.Parse() function.
-        Console.Write("What is your grade percentage? ");
+        Console.Write("\nWhat is your grade percentage? ");
         string userInput = Console.ReadLine();
         int gradePercentage = int.Parse(userInput);
         // Test to be sure the user interaction and variable works.
@@ -56,6 +56,8 @@ class Program
             letter = "F";
         }
 
+
+        Console.WriteLine($"Your grade is: {letter}\n");
 
 
         // Step 1 & 2:
