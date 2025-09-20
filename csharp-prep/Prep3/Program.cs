2c96db544236232bf4e50363b28a090d4ed050ee
rmharden <har22079@byui.edu>
2025-09-20 10:23:10 -0400

    Working on step 1. The do while and while loops are repeating the display message. I need to find a better way to write this.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -10,6 +10,27 @@ class Program
         int magicNumber = int.Parse(userInput);
 
         // Test:
-        Console.WriteLine($"\nYou entered: {magicNumber}");
+        // Console.WriteLine($"\nYou entered: {magicNumber}");
+
+        Console.WriteLine("What is your guess? ");
+        string userpInput2 = Console.ReadLine();
+        int guess = int.Parse(userpInput2);
+
+        do
+        {
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("\nLower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
+     
     }
 }
