03595118b4e76ae3197a711cc62a6a349afba2b7
rmharden <har22079@byui.edu>
2025-09-20 11:03:10 -0400

    Added a random generator for the magic number. It works!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,15 +5,22 @@ class Program
     static void Main(string[] args)
     {
         // Step 1:
+        /*
         Console.Write("\nWhat is the magic number? ");
         string userInput = Console.ReadLine();
         int magicNumber = int.Parse(userInput);
+        */
 
         // Test:
         // Console.WriteLine($"\nYou entered: {magicNumber}");
 
+        // Step 3
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
         int guess = -1;
 
+        // Step 2:
         while (guess != magicNumber)
         {
             Console.WriteLine("What is your guess? ");
