cbcac8a8efd709b137a0670bd8640fa6a1296e5c
rmharden <har22079@byui.edu>
2025-09-20 11:16:47 -0400

    Created a user interaction and parsed the user response

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        int response = -1;
+
+        while (response != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished.");
+            response = int.Parse(Console.ReadLine());
+        }
     }
 }
