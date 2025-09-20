a5e01f186438aec6be141dca64e5a13383d9b69b
rmharden <har22079@byui.edu>
2025-09-20 13:09:34 -0400

    followed example on adding a if statement for adding the responses to the list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,20 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         // Step 1:
+        List<int> numbers = new List<int>();
 
-        List<int> numbers;
-        numbers = new List<int>();
-        
         int response = -1;
 
         while (response != 0)
         {
-            Console.Write("Enter a list of numbers, type 0 when finished.");
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            Console.Write("Enter number: ");
+
             response = int.Parse(Console.ReadLine());
+
+            if (response != 0)
+            {
+                numbers.Add(response);
+            }
         }
+        
+        for (int i = 0; )
     }
 }
