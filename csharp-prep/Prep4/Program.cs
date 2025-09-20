fc665cf8171e3635e33db975ff618f1137c13abd
rmharden <har22079@byui.edu>
2025-09-20 14:16:09 -0400

    Moved the initial displayed statement. It repeats and I don't want that part in the loop.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,13 +6,14 @@ class Program
     static void Main(string[] args)
     {
         // Step 1:
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
         List<int> numbers = new List<int>();
 
         int response = -1;
 
         while (response != 0)
         {
-            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
             Console.Write("Enter number: ");
 
             response = int.Parse(Console.ReadLine());
