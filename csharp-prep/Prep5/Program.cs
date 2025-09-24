a34efb2bcb5bda5cc5e850386661e1c83ee2c687
rmharden <har22079@byui.edu>
2025-09-23 22:00:47 -0400

    Adding code to the main function to call the other functions.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        DisplayWelcome();
+
+        string userName = PropmptUserName();
     }
     static void DisplayWelcome()
     {
