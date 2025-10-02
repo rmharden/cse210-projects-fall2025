5014605a48e0784020203d47c34d8c8e03c899b2
rmharden <har22079@byui.edu>
2025-10-02 16:33:04 -0400

    Wrote a menu in the main program. The loop is mostly working, expecpt when an option is chosen, its not displaying the appropriate message.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("\nWelcome to the Journal Program!");
+
+
+        string _userChoice = "";
+        // Menu
+        while (_userChoice != "5")
+        {
+            Console.WriteLine("Please select one of the following choices");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("What would you like to do? ");
+
+            _userChoice = Console.ReadLine();
+
+            // 1. Write
+            if (_userChoice == "1")
+            {
+
+            }
+
+            // 2. Display
+            else if (_userChoice == "2")
+            {
+                Console.WriteLine($"You chose option {_userChoice}");
+            }
+
+            // 3. Load
+            else if (_userChoice == "3")
+            {
+                Console.WriteLine($"You chose option {_userChoice}");
+            }
+
+            // 4. Save
+            else if (_userChoice == "4")
+            {
+                Console.WriteLine($"You chose option {_userChoice}");
+            }
+
+            // 5. Quit
+            else if (_userChoice == "5")
+            {
+                Console.WriteLine("Thanks for using the Journal Program!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number\n");
+            }
+        }
     }
 }
