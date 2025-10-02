d6ffc2d3d4e703cc94a8436d5105cd157b063c33
rmharden <har22079@byui.edu>
2025-10-02 10:20:48 -0400

    Wrote the Resume.cs class.

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,4 +1,17 @@
+using System;
 public class Resume
 {
-    
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Name: {_jobs}");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
 }
