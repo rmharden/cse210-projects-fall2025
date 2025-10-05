2828a129a6caff07142772afbac24b2435ec9668
rmharden <har22079@byui.edu>
2025-10-05 16:18:18 -0400

    needed to make the userInput variable a string not an int

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,17 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Journal Program!");
-        int userInput = -1;
+        string userInput = "";
 
-        while (userInput != 5)
+        while (userInput != "5")
         {
-            Console.Write("Please select one of the following choices: ");
+            Console.WriteLine("Please select one of the following choices:");
 
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
             Console.WriteLine("3. Load");
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
+
+            Console.Write("What would you like to do? ");
+            userInput = Console.ReadLine();
         }
     }
 }
