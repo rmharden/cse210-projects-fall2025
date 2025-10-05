4926f45b767db6d51aaefb3ec41926a17ab37d2a
rmharden <har22079@byui.edu>
2025-10-05 16:41:40 -0400

    wrote an instance for the list of _prompts and created a string for the method

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,6 +1,10 @@
 public class PropmptGenerator
 {
-
+    List<string> _prompts = new List<string>();
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
 }
 
 // https://video.byui.edu/media/t/1_vmeovorz
