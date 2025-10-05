ec9c38b2e76f2ce326acec95fe1b8141f3de5808
rmharden <har22079@byui.edu>
2025-10-05 16:38:09 -0400

    Wrote member variables and started the method for display in the Entry.cs class

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+    public void Display()
+    {
+        
+    }
+}
+
+// https://video.byui.edu/media/t/1_vmeovorz
