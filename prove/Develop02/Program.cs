f0e5b7cd21e4fc972ee5128b890e5b1f576cc81b
rmharden <har22079@byui.edu>
2025-10-05 16:04:24 -0400

    Started a while loop for the menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Journal Program!");
+        int userInput = -1;
+
+        while (userInput != 5)
+        { }
     }
 }
