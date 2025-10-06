b7293a1e456a068a41b4fb1c23d88ee5ae3eaf06
rmharden <har22079@byui.edu>
2025-10-06 08:15:20 -0400

    added the list to  the prompt generator and begain writing a random prompt ... I don't know how to access the list randomly

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,8 +1,19 @@
+using System;
+
 public class PropmptGenerator
 {
-    public List<string> _prompts = new List<string>();
+    public List<string> _prompts = new List<string>()
+    {
+       "Who was the most interesting person I interacted with today?",
+       "What was the best part of my day?",
+       "How did I see the hand of the Lord in my life today?",
+       "What was the strongest emotion I felt today?",
+       "If I had one thing I could do over today, what would it be?"
+    };
     public string GetRandomPrompt()
     {
+        Random randomPrompt = new Random();
+
         return "";
     }
 }
