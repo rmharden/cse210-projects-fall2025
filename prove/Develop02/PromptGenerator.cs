90295b5ac18a19e92363215d8390a9a801e67759
rmharden <har22079@byui.edu>
2025-10-06 08:27:40 -0400

    Wrote the PromptGenerator class with comments.

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -13,9 +13,15 @@ public class PropmptGenerator
     public string GetRandomPrompt()
     {
         Random randomPrompt = new Random();
+        int i = randomPrompt.Next(_prompts.Count);
 
-        return "";
+        return _prompts[i];
     }
 }
 
 // https://video.byui.edu/media/t/1_vmeovorz
+
+// https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-9.0#examples
+// The example I used for accessing the list was under: "The following example generates a random integer that uses an index to retrieve a string value from an array." I used a list. They also had ".Length" but our lesson says to use "Count" not "Length,"(because length, I thought, was Python?) so I used count. I don't know if this will work 😭
+
+// Returning the index of the _prompts, or "return _prompts[i] came from our lesson about iterating through a List. This same area in our less also has examples of using Count with the instance of the list.
