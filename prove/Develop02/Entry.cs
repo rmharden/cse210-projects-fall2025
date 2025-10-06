dcbc54e1951c865c431129e2b465fd9c716aa25f
rmharden <har22079@byui.edu>
2025-10-06 09:05:14 -0400

    Wrote the display message in the Entry.cs class

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -7,7 +7,7 @@ public class Entry
     public string _entryText;
     public void Display()
     {
-        
+        Console.WriteLine($"\nDate: {_date} - Prompt: {_promptText}\n{_entryText}");
     }
 }
 
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -22,6 +22,8 @@ public class PropmptGenerator
 // https://video.byui.edu/media/t/1_vmeovorz
 
 // https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-9.0#examples
+
+
 // The example I used for accessing the list was under: "The following example generates a random integer that uses an index to retrieve a string value from an array." I used a list. They also had ".Length" but our lesson says to use "Count" not "Length,"(because length, I thought, was Python?) so I used count. I don't know if this will work 😭
 
 // Returning the index of the _prompts, or "return _prompts[i] came from our lesson about iterating through a List. This same area in our lesson also has examples of using Count with the instance of the list.
