57ca77b7cf2facad04767ba1a3c9b2f2f8d58b42
rmharden <har22079@byui.edu>
2025-10-11 14:38:47 -0400

    Called the GetFractionString for each fraction instance in the main Program.cs.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -6,10 +6,13 @@ class Program
     {
         // Step 4
         Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
 
         Fraction f2 = new Fraction(6);
+        Console.WriteLine(f2.GetFractionString());
 
         Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine(f3.GetFractionString());
 
 
     }
