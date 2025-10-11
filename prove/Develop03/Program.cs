bf71a6501d7fa26b089e36c44628de07c517c3e0
rmharden <har22079@byui.edu>
2025-10-11 14:50:10 -0400

    Wrote comments in the Scripture Memorizer main Program.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,19 @@
+/*
+Rebekah Harden
+Brother Randall Booth
+CSE 210: Programming with Classes
+October 11, 2025
+
+W06 Prove: Developer
+Unit 03 Develop: Scripture Memorizer
+*/
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Hello Develop03: Scripture Memorizer World!");
     }
 }
