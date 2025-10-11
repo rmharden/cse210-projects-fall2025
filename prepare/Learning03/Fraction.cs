c9c21986bfe4fd8cc091a9e3d7012269e23343d6
rmharden <har22079@byui.edu>
2025-10-11 14:21:46 -0400

    Wrote comments for steps 1-3.

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -2,21 +2,25 @@ using System.Diagnostics;
 
 public class Fraction
 {
+    // Step 3:
     private int _top;
     private int _bottom;
 
+    // Step 4:
     public Fraction()
     {
+        // Constructor that has no parameters and initializes the number to 1/1.
         _top = 1;
         _bottom = 1;
-
     }
+    // Constructor that has one parameters for the numerator and initializes the denominator as 1.
     public Fraction(int wholeNumber)
     {
         _top = wholeNumber;
         _bottom = 1;
     }
 
+    // Consturctor that has two parameters for both the numerator and the demominator.
     public Fraction(int bottom, int top)
     {
         _bottom = bottom;
