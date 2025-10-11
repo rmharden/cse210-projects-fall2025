c4c22fbf56be94b45c30f9cde906898be175503b
rmharden <har22079@byui.edu>
2025-10-11 14:07:03 -0400

    Wrote stubs in Fraction. Not sure if it is correct yet.

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,6 +1,47 @@
-public class Fraction
+using System.Diagnostics;
+
+public class Fractions
 {
     private int _top;
     private int _bottom;
-    
+
+    private void Fraction()
+    {
+
+    }
+    private void Fraction(int wholeNumber)
+    {
+        wholeNumber = 1;
+    }
+
+    private void Fraction(int bottom, int top)
+    {
+        _bottom = bottom;
+        _top = top;
+    }
+
+    private int GetTop()
+    {
+        return _top;
+    }
+    private void SetTop(int top)
+    {
+        _top = top;
+    }
+    private int GetBottom()
+    {
+        return _bottom;
+    }
+    private void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    private string GetFractionString()
+    {
+        return "";
+    }
+    private double GetDecimalValue()
+    {
+        return 0;
+    }
 }
