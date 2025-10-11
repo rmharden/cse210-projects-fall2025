fa46c1992cfe34de9d9a413e58467d60bbd81b26
rmharden <har22079@byui.edu>
2025-10-11 14:42:40 -0400

    I wrote the test code but my numerator and denominators are reversed... need to find out why.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,16 +5,20 @@ class Program
     static void Main(string[] args)
     {
         // Step 4 & Step 6
-        Fraction f1 = new Fraction();
+        Fraction f1 = new Fraction(1);
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
 
-        Fraction f2 = new Fraction(6);
+        Fraction f2 = new Fraction(5);
         Console.WriteLine(f2.GetFractionString());
         Console.WriteLine(f2.GetDecimalValue());
 
-        Fraction f3 = new Fraction(6, 7);
+        Fraction f3 = new Fraction(3,4);
         Console.WriteLine(f3.GetFractionString());
         Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1,3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
