8b034654ce30bdda56f36f180d104188710af6e8
rmharden <har22079@byui.edu>
2025-10-13 12:43:28 -0400

    I wrote the code for the Reference class. I'm not sure it's complete, but this is my first attempt.

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,4 +1,29 @@
 public class Reference
 {
-    
+    private string _book;
+    private int _startVerse;
+    private int _endVerse;
+    public Reference(string book, int startVerse)
+    {
+        _book = book;
+        _startVerse = startVerse;
+        _endVerse = _startVerse;
+    }
+    public Reference(string book, int startVerse, int endVerse)
+    {
+        _book = book;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
+    }
+    public string DisplayReference()
+    {
+        if (_endVerse != _startVerse)
+        {
+            return $"{_book}:{_startVerse}-{_endVerse}";
+        }
+        else
+        {
+            return $"{_book}:{_startVerse}";
+        }
+    }
 }
