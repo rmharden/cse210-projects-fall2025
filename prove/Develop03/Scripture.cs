be9849adc8b2018c9c7c3a033bc9677f2cd9ece4
rmharden <har22079@byui.edu>
2025-10-13 16:24:52 -0400

    Wrote more comments.

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -10,7 +10,20 @@ public class Scripture
         _verse = verse;
     }
 
+    public void HideWords()
+    {
+        // I don't know if this method is void yet. I put it this way for now.
+    }
 
+    public bool IsCompletelyHidden()
+    {
+        // I'm guessing here. I don't know how to do this. Is this where an if statement goes. Also, I don't understand booleans very well, but I think a boolean goes here.
+        return false;
+    }
+
+    
+
+    // Is this method the same thing as "Get Rendered Text"?
     public string DisplayScripture()
     {
         return $"{_reference.DisplayReference()} {_verse}";
