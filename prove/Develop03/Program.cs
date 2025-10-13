f780a623e3357ed7167ec4f34f7f86288abac779
rmharden <har22079@byui.edu>
2025-10-13 18:02:11 -0400

    Wrote test code for the Word class.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -14,29 +14,36 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        // Test code for Word. Creating a word object:
+        // Word w = new Word("truth");
+        // Console.WriteLine(w.GetRenderedText());
+
+
+
         string _john = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
-        Reference r1 = new Reference("John", 3, 6);
-        Scripture s1 = new Scripture(r1, _john);
+            Reference r1 = new Reference("John", 3, 6);
+            Scripture s1 = new Scripture(r1, _john);
 
-        string _proverbs = "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknoledge him, and he shall direct thy paths.";
-        Reference r2 = new Reference("Proverbs", 3, 5, 6);
-        Scripture s2 = new Scripture(r2, _proverbs);
+            string _proverbs = "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknoledge him, and he shall direct thy paths.";
+            Reference r2 = new Reference("Proverbs", 3, 5, 6);
+            Scripture s2 = new Scripture(r2, _proverbs);
 
-        string userInput = "";
-        while (userInput.ToLower() != "quit")
-        {
+            string userInput = "";
+            while (userInput.ToLower() != "quit")
+            {
+                Console.Clear();
+
+                Console.WriteLine($"{s1.DisplayScripture()}\n");
+
+                Console.WriteLine($"{s2.DisplayScripture()}\n");
+
+                Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+                userInput = Console.ReadLine();
+            }
             Console.Clear();
-
-            Console.WriteLine($"{s1.DisplayScripture()}\n");
-
-            Console.WriteLine($"{s2.DisplayScripture()}\n");
-
-            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
-            userInput = Console.ReadLine();
         }
-        Console.Clear();
     }
-}
 
 // Used this site to learn how to make the user input all lower case:
 // https://learn.microsoft.com/en-us/dotnet/api/system.string.tolower?view=net-9.0#system-string-tolower
