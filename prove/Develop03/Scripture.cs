d39627510f0dfb6a0982e456d43e92bde1568428
rmharden <har22079@byui.edu>
2025-10-13 14:25:22 -0400

    Took out getters and setters for verse because I didn't need them.

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -7,14 +7,6 @@ public class Scripture
         _reference = reference;
         _verse = verse;
     }
-    // public string GetVerse()
-    // {
-    //     return _verse;
-    // }
-    // public void SetVerse(string verse)
-    // {
-    //     _verse = verse;
-    // }
     public string DisplayScripture()
     {
         return $"{_reference.DisplayReference()} {_verse}";
