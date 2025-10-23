f27d76d71736641e5d66bb25f9eab47106f1cb0f
rmharden <har22079@byui.edu>
2025-10-23 17:09:33 -0400

    wrote private member variables

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,8 +1,11 @@
 public class Scripture
 {
-    
+    private Reference _reference;
+    private string _verse;
+    private List<Word> _words;
     public Scripture(Reference reference, string verse)
     {
+        
 
     }
 }
