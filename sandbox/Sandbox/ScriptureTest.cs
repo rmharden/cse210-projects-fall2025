4f84598fb7de437f6004e7755136536d3fb87c4c
rmharden <har22079@byui.edu>
2025-10-25 14:42:58 -0400

    Found a way to split the verse on Microsoft Learn

--- sandbox/Sandbox/ScriptureTest.cs
+++ sandbox/Sandbox/ScriptureTest.cs
@@ -1,4 +1,30 @@
+using System.Dynamic;
+
 public class ScriptureTest
 {
-    
+    private List<WordTest> _words = new List<WordTest>();
+    private ReferenceTest _referenceTest;
+    private string _verse;
+
+    public ScriptureTest(ReferenceTest referenceTest, string verse)
+    {
+        _referenceTest = referenceTest;
+        _verse = verse;
+
+        string[] _splitWords = _verse.Split(' ');
+        for (int i = 0; i < _splitWords.Length; i++)
+        {
+            WordTest _aWord = new WordTest(_splitWords[i]);
+            _words.Add(_aWord);
+        }
+
+    }
 }
+
+/*
+Sites I used for help:
+
+https://learn.microsoft.com/en-us/dotnet/csharp/how-to/parse-strings-using-split
+
+
+*/
