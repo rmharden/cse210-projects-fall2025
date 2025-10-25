4f177af8b97fcec294afaa35cbef41d5dcd49741
rmharden <har22079@byui.edu>
2025-10-25 12:22:15 -0400

    initializing member variables in the reference class

--- sandbox/Sandbox/Reference.cs
+++ sandbox/Sandbox/Reference.cs
@@ -1,4 +1,12 @@
 public class Reference
 {
+    private string _book;
+    private int _chapter;
+    private int _startVerse;
+    private int _endVerse;
     
+    public Reference(string book, int chapter, int startVerse)
+    {
+        _book = book;
+    }
 }
