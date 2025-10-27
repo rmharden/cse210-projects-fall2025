895800f460abeec657fbae884a153bead3f91bfa
rmharden <har22079@byui.edu>
2025-10-27 14:46:20 -0400

    Changed a lowercase to an uppercase.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,8 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Assignment anAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine();
         Console.WriteLine(anAssignment.GetSummary());
 
-        MathAssignment aMathAssignment = new MathAssignment("Roberto Rodriquez", "Fractions", "Section 7.3", "problems 8-19");
+        MathAssignment aMathAssignment = new MathAssignment("Roberto Rodriquez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine($"\n{aMathAssignment.GetHomeWorkList()}\n");
     }
 }
