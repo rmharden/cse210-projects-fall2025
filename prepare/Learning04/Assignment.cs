7f4f810e1c53be1c0580708ce37fad5a05c36ba0
rmharden <har22079@byui.edu>
2025-10-27 13:51:07 -0400

    Wrote member variables and made a constructor with parameters in Assignment

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,10 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _type;
+    public Assignment(string studentName, string type)
+    {
+        _studentName = studentName;
+        _type = type;
+    }
+}
