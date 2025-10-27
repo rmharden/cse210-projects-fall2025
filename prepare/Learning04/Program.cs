bece8633e7541ced51bb6c0fa5994f3b9c1a30f4
rmharden <har22079@byui.edu>
2025-10-27 14:32:49 -0400

    Made a MathAssignment object and set values in the main program.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,5 +6,7 @@ class Program
     {
         Assignment anAssignment = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(anAssignment.GetSummary());
+
+        MathAssignment aMathAssignment = new MathAssignment("Roberto Rodriquez", "Fractions", "Section 7.3", "problems 8-19");
     }
 }
