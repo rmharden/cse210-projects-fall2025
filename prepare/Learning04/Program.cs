ed9af2ca2fbbdca6906d5924ca90b476017f6cb0
rmharden <har22079@byui.edu>
2025-10-27 14:59:48 -0400

    Wrote the code - however, It said that I couldn't access the _studentName variable in the base class unless I change it to protected or I create getters and setters. The code works without them... Do I need to do that so the code keeps everything private?

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -10,5 +10,8 @@ class Program
 
         MathAssignment aMathAssignment = new MathAssignment("Roberto Rodriquez", "Fractions", "Section 7.3", "Problems 8-19");
         Console.WriteLine($"\n{aMathAssignment.GetHomeWorkList()}\n");
+
+        WritingAssignment aWritingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine($"\n{aWritingAssignment.GetWritingInformation()}\n");
     }
 }
