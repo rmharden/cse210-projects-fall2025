4a95f2303a2de8138e13e013ac3f0e34f4e4be3e
rmharden <har22079@byui.edu>
2025-10-27 14:51:09 -0400

    Wrote the inheritence and set variables for the WritingAssignment class.

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,8 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+}
