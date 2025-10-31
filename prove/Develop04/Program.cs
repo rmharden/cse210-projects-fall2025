5d2f69cb2a1eea286c609cc210fd02f107e8f305
rmharden <har22079@byui.edu>
2025-10-31 12:25:02 -0400

    Wrote comments in the main Program.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,3 +1,12 @@
+/*
+Rebekah Harden
+CSE 210: Programming with Classes
+Brother Randall Booth
+October 31, 2025
+
+W08 Prove: Developer
+*/
+
 using System;
 
 class Program
@@ -7,3 +16,11 @@ class Program
         Console.WriteLine("Hello Develop04 World!");
     }
 }
+
+/*
+
+BYU-Idaho Video - Brother Chad Macbeth's CSE 210: Week 4 Assignment Video:
+https://video.byui.edu/media/t/1_f4017z1d/259941952
+
+
+*/
