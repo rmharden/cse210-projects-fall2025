0fc668f67670b35d37bf2cfbc7577b7ea2415346
rmharden <har22079@byui.edu>
2025-10-31 13:54:31 -0400

    wrote some code to try in the base class.

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -5,11 +5,19 @@ public class Activity
     private int _duration;
     public Activity()
     {
+        _activityName = "";
+        _description = "";
+        _duration = 0;
 
     }
     public void DisplayStartingMessage()
     {
-
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_activityName}");
+        Console.WriteLine();
+        string input = Console.ReadLine();
+        int _duration = int.Parse(input);
+        ShowSpinner(_duration);
     }
     public void DisplayEndingMessage()
     {
