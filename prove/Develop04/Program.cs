07f9e42522cf7b7d47a55b9732192bfec3fb1481
rmharden <har22079@byui.edu>
2025-11-11 12:23:52 -0500

    Re-writing the main program.

---
+++ prove/Develop04/Program.cs
@@ -0,0 +1,19 @@
+/*
+Rebekah Harden
+Brother Randall Booth
+CSE 210: Programming with Classes
+November 3, 2025
+
+W08 Prove: Developer
+Unit 04 Develop: Mindfulness Program
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
