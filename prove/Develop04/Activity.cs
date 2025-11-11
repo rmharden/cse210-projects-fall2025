9c6c400a8a41bd375591c8638f00e509dd9329c5
rmharden <har22079@byui.edu>
2025-11-11 13:11:53 -0500

    Wrote a start message in the base class using the member variables.

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -9,21 +9,23 @@ public class Activity
         _description = description;
         _duration = duration;
     }
-    public string GetActivityName()
-    {
-        return _activityName;
-    }
-    public string GetDescription()
-    {
-        return _description;
-    }
-    public int GetDuration()
-    {
-        return _duration;
-    }
+    // public string GetActivityName()
+    // {
+    //     return _activityName;
+    // }
+    // public string GetDescription()
+    // {
+    //     return _description;
+    // }
+    // public int GetDuration()
+    // {
+    //     return _duration;
+    // }
     public void DisplayStartMessage()
     {
-
+        Console.WriteLine($"Welcome to the {_activityName} Activity");
+        Console.WriteLine($"\nThis activity will help you {_description}");
+        Console.WriteLine("\nHow long in seconds would like your session for? ");
     }
     public void DisplayEndMessage()
     {
