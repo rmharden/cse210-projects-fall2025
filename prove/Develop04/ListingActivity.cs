9cdf1f0a57688f6c96fc694d9275a3d9c4316276
rmharden <har22079@byui.edu>
2025-11-11 13:00:05 -0500

    Wrote stubs for ListingActivity.cs. Not sure about the list. I don't know if I can have it as a data type. I also have parameters being passed through the constructor and I don't know if they go there.

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -2,12 +2,21 @@ public class ListingActivity
 {
     int _count;
     List<string> _prompts;
-    public ListingActivity()
+    public ListingActivity(int count, List<string> prompts)
     {
-
+        _count = count;
+        _prompts = prompts;
     }
     public void Run()
     {
-        
+
+    }
+    public void GetRandomPrompt()
+    {
+
+    }
+    public List<string> GetListFromUser()
+    {
+        return _prompts;
     }
 }
