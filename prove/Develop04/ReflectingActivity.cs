002fa1fc11d21d8170306f1171af579813bfe536
rmharden <har22079@byui.edu>
2025-11-11 13:03:03 -0500

    Wrote stubs for the ReflectingClass with member variables and methods.

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -1,10 +1,28 @@
 public class ReflectingActivity
 {
+    private List<string> _prompts;
+    private List<string> _questions;
     public ReflectingActivity()
     {
 
     }
     public void Run()
+    {
+
+    }
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+    public void DisplayPrompt()
+    {
+
+    }
+    public void DisplayQuestion()
     {
         
     }
