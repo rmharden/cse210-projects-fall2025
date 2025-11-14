be0cae4e51f76e77fc00497ad5f67a7331bafd58
rmharden <har22079@byui.edu>
2025-11-13 19:49:11 -0500

    Wrote code to return the area in the GetArea method.

--- prepare/Learning05/Square.cs
+++ prepare/Learning05/Square.cs
@@ -1,9 +1,17 @@
 // Step 4: Create the Square class
+using System.Security.Cryptography.X509Certificates;
+
 public class Square : Shape
 {
     private float _side;
     public Square(string color, float side) : base(color)
     {
         _side = side;
-    }    
+    }
+    public override float GetArea()
+    {
+        float _area = 0;
+        _area = _side * _side;
+        return _area;
+    }   
 }
