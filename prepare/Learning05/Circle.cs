2b44baa9a24844716d36d26852d5b498ef814b2a
rmharden <har22079@byui.edu>
2025-11-13 20:29:44 -0500

    Fixed the member variable. had the wrong assignment for radius.

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -5,7 +5,7 @@ public class Circle : Shape
     private double _radius;
     public Circle(string color, double radius) : base(color)
     {
-        radius = _radius;
+        _radius = radius;
     }
     public override double GetArea()
     {
