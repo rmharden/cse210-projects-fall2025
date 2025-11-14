b8fc5dbc31e142a75f66bac84d0bcd74f7b1ac90
rmharden <har22079@byui.edu>
2025-11-14 16:35:41 -0500

    Writing basic test code in the SimpleGoal class that will probably go into a base class.

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -1,7 +1,12 @@
 public class SimpleGoal
 {
+    string _goalName;
+    string _goalDescription;
     public SimpleGoal()
     {
-        
+        Console.Write("What is the name of your goal? ");
+        _goalName = Console.ReadLine();
+        Console.Write("What is the a short description of it? ");
+        _goalDescription = Console.ReadLine();
     }
 }
