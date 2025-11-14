f5df5ad171f6d7264f89d0ccc8ad1999d1edb2be
rmharden <har22079@byui.edu>
2025-11-13 20:36:18 -0500

    Step 7: created a list, added the shapes, and wrote code to iterate through that list.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -16,19 +16,33 @@ class Program
     {
         // Console.WriteLine("Hello Learning05 World!");
 
+        // Step 7: Build a List
+        List<Shape> _shapes = new List<Shape>();
+
         // Step 5: Test the Square class
         Square s1 = new Square("yellow", 5);
-        Console.WriteLine(s1.GetColor());
-        Console.WriteLine(s1.GetArea());
+        // Console.WriteLine(s1.GetColor());
+        // Console.WriteLine(s1.GetArea());
 
         //Step 6: Test the Rectangle class
         Rectangle r1 = new Rectangle("orange", 15, 10);
-        Console.WriteLine(r1.GetColor());
-        Console.WriteLine(r1.GetArea());
+        // Console.WriteLine(r1.GetColor());
+        // Console.WriteLine(r1.GetArea());
 
         // Step 6: Test the Circle class
         Circle c1 = new Circle("red", 5);
-        Console.WriteLine(c1.GetColor());
-        Console.WriteLine(c1.GetArea());
+        // Console.WriteLine(c1.GetColor());
+        // Console.WriteLine(c1.GetArea());
+
+        // Step 7: Build a List
+        _shapes.Add(s1);
+        _shapes.Add(r1);
+        _shapes.Add(c1);
+
+        foreach (Shape shape in _shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
