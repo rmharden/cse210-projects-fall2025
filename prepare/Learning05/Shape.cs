8e2f80460806c1d41459d8f747e8a71fd4309639
rmharden <har22079@byui.edu>
2025-11-13 19:36:00 -0500

    Wrote a constructor for the Shape class and accepts the color and sets it.

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,13 +1,17 @@
 public class Shape
 {
     private string _color;
+    public Shape(string color)
+    {
+        _color = color;
+    }
     public string GetColor()
     {
         return _color;
     }
     public void SetColor()
     {
-        
+
     }
     
 }
