f62280ca510ee0aa229ed1146c1b6256b7ae0414
rmharden <har22079@byui.edu>
2025-11-14 11:28:17 -0500

    Wrote a comment with my header for the assignment.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,10 +1,21 @@
+/*
+Rebekah Harden
+CSE 210: Programming with Classes
+Brother Randall Booth
+November 14, 2025
+
+W10 Prove: Developer
+Unit 05 Develop: Eternal Quest Program
+*/
+
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        // Console.WriteLine("Hello Develop05 World!");
         EternalQuestGame e1 = new EternalQuestGame(0);
         e1.Menu();
     }
