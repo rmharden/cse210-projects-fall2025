2694875e96a353f3eb5942f3dcfedad40b983940
rmharden <har22079@byui.edu>
2025-11-14 11:05:42 -0500

    Wrote a menu for the Eternal Quest program in the Main class in the Program class.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop05 World!");
+        Console.Clear();
+        var choice = "";
+        do
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(" 1.  Create New Goal");
+            Console.WriteLine(" 2.  List Goals");
+            Console.WriteLine(" 3.  Save Goals");
+            Console.WriteLine(" 4.  Load Goals");
+            Console.WriteLine(" 5.  Record Event");
+            Console.WriteLine(" 6.  Quit");
+            Console.Write("Select a choice from the menu: ");
+            choice = Console.ReadLine();
+            if (choice == "1")
+            {
+
+            }
+            else if (choice == "2")
+            {
+
+
+            }
+            else if (choice == "3")
+            {
+
+
+            }
+            else if (choice == "4")
+            {
+
+
+            }
+            else if (choice == "5")
+            {
+
+
+            }
+        } while (choice != "6");
     }
 }
