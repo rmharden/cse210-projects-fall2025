b1fe9b070c44debfcccdd27e9b97630bc71fa765
rmharden <har22079@byui.edu>
2025-11-14 16:39:31 -0500

    Wrote a few displayed messages and variables to save user input.

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -2,11 +2,14 @@ public class SimpleGoal
 {
     string _goalName;
     string _goalDescription;
+    double _points;
     public SimpleGoal()
     {
         Console.Write("What is the name of your goal? ");
         _goalName = Console.ReadLine();
         Console.Write("What is the a short description of it? ");
         _goalDescription = Console.ReadLine();
+        Console.Write("What is the amount of points associated with this goal? ");
+        int _points = int.Parse(Console.ReadLine());
     }
 }
