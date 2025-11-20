9d734244bc7a318bfd16c9bbfc2365c49151b40e
rmharden <har22079@byui.edu>
2025-11-20 10:44:20 -0500

    Writing notes in the GoalManger.cs

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -8,22 +8,33 @@ public class GoalManager
         _score = score;
     }
     public void Start()
+    // display the main menu
+    // call:
+    // CreateGoal, ListGoalDetails, SaveGoals, LoadGoals, RecordEvent, Quit
     {
 
     }
     public void DisplayPlayerInfo()
+    // Display the points
     {
 
     }
     public void ListGoalNames()
+    // Loop through the list of goals and display the names (_shortName)
+    // YOU NEED ANOTHER FUNCTION IN THE GOAL CLASS TO BE ABLE TO CALL THE NAMES FROM THE LIST IN GOAL.
     {
 
     }
     public void ListGoalDetails()
+    // Loop through the list of goals and display the full details
+    // Call the Get GetDetails string function
     {
 
     }
     public void CreateGoal()
+    // Three possible goals we can create
+    // Ask the user what type of goal they want to create.
+    // Display a sub-menu to select a goal type.
     {
 
     }
@@ -37,6 +48,6 @@ public class GoalManager
     }
     public void LoadGoals()
     {
-        
+
     }
 }
