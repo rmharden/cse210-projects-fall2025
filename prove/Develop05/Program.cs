5579680f1b8e3de41637ebbdde57adb7685ff1b2
rmharden <har22079@byui.edu>
2025-11-20 09:30:27 -0500

    I saved my code in word document and I'm starting over. I got confused.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -15,8 +15,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Develop05 World!");
-        EternalQuestGame e1 = new EternalQuestGame(0);
-        e1.Menu();
+
     }
 }
