c5077ffad1304613cb8c3a8c10454131845100f4
rmharden <har22079@byui.edu>
2025-11-21 12:10:54 -0500

    Wore the menu options.

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -14,6 +14,22 @@ public class GoalManager
     {
         Console.Clear();
 
+        var choice = "";
+        do
+        {
+            Console.WriteLine($"You have {_score} points.");
+
+            Console.WriteLine("\nMenu Options");
+            Console.WriteLine("1.   Create New Goal");
+            Console.WriteLine("2.   List Goals");
+            Console.WriteLine("3.   Save Goals");
+            Console.WriteLine("4.   Load Goals");
+            Console.WriteLine("5.   Record Event");
+            Console.WriteLine("6.   Quit");
+            Console.Write("Select a choice from the menu: ");
+        } while (choice != "6");
+
+
     }
     public void DisplayPlayerInfo()
     {
