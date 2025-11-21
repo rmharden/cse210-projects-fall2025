3a3df866bf27b16e8d01a54f22ea8357d97fd21f
rmharden <har22079@byui.edu>
2025-11-21 16:05:49 -0500

    Deleted comments

--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -22,11 +22,3 @@ public class SimpleGoal : Goal
         return $"You have {_points} points.\n [X] {_shortName} ({_description})";
     }
 }
-
-/*
-    Console.Writeline("Which type of goal would you like to create?");
-    Console.Writeline("What is the name of your goal?");
-    Console.Writeline("What is a short description of it?");
-    Console.Writeline("What is the amount of points associated with this goal?");
-    
-*/
