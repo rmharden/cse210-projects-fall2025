7822b382c64a546c0c0c40af1102532a0dfffe4f
rmharden <har22079@byui.edu>
2025-11-26 11:29:26 -0500

    Wrote member variables in the Goal base class.

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -5,3 +5,16 @@
 3. Follow the principles of encapsulation and abstraction by having private member variables and putting reltated items in the same class.
 
 */
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+}
