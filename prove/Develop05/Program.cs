daef4e2bd7ba478ae769b6abc87eebaafbb65027
rmharden <har22079@byui.edu>
2025-11-26 11:13:38 -0500

    Wrote my header in the main program - and my references section.

---
+++ prove/Develop05/Program.cs
@@ -0,0 +1,23 @@
+/*
+Rebekah Harden
+CSE 210: Programming with Classes
+Brother Randall Booth
+November 14, 2025
+
+W10 Prove: Developer
+Polymorphism Development Project
+Unit 05 Develop: Eternal Quest Program
+*/
+
+
+
+
+
+/* 
+Sites I used for help:
+
+BYU-Idaho Videos
+Chad Macbeth CSE:210 Week 5 Assignment
+https://video.byui.edu/media/t/1_6lpgm7dz
+
+*/
