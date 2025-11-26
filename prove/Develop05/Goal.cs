78fc7996ec452adfd9244e10b37cd9f8fe50eb07
rmharden <har22079@byui.edu>
2025-11-26 12:05:29 -0500

    Wrote a string for the GetDetailString method.

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -17,11 +17,33 @@ public abstract class Goal
         _description = description;
         _points = points;
     }
+    /*
+    This is from the lessons from the CSE 210 7 Week course:
+
+    "Each goal needs to provide a way to record the event.
+    Each one needs to provide a check to see if it IS COMPLETE.
+    This method should do whatever necessary for each specific kind of goal, such as making a simple goal complete and adding to the number of times a checklist goal has been completed. It should return the point value associated with recording the event. (Keep in mind that it may contain a bonus in some cases if a checklist goal was just finished.)"
+    */
     public abstract void RecordEvent();
+
+    /*
+        This method should return true if the goal is gompleted. The way you determine if a goal scomplete is different for each type of goal.
+    */
     public abstract bool IsComplete();
+
+    /*
+    This method should return the details of a goal that could be shown in a LIST. It should include the checkbox
+    */
     public string GetDetailString()
     {
-        return "";
+        // if (IsComplete == true)
+        // {
+        //     return $"[X] {_shortName} ({_description})";
+        // }
+        // else
+        // {
+            return $"[ ] {_shortName} ({_description})";
+        // }
     }
     public abstract string GetStringRepresentation();
 }
