ffad455ec5c1c7e5d86db4ab858c9517084b103e
rmharden <har22079@byui.edu>
2025-11-26 17:40:39 -0500

    Called methods from each menu choice if else statement.

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -22,7 +22,40 @@ public class GoalManager
         var choice = "";
         do
         {
-            
+            DisplayPlayerInfo();
+
+            Console.WriteLine("\nMenu Options");
+            Console.WriteLine("   1. Create New Goal");
+            Console.WriteLine("   2. List Goals");
+            Console.WriteLine("   3. Save Goals");
+            Console.WriteLine("   4. Load Goals");
+            Console.WriteLine("   5. Record Event");
+            Console.WriteLine("   6. Quit");
+            Console.Write("Selecct a choice from the meun:");
+
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                CreateGoal();
+            }
+            else if (choice == "2")
+            {
+                ListGoalDetails();
+            }
+            else if (choice == "3")
+            {
+                SaveGoals();
+            }
+            else if (choice == "4")
+            {
+                LoadGoals();
+            }
+            else if (choice == "5")
+            {
+                RecordEvent();
+            }
+
             Console.WriteLine();
         } while (choice != "6");
     }
