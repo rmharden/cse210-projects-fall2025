6784bd6f6e5ce57f0eee2daa3f7d278db9231e78
rmharden <har22079@byui.edu>
2025-11-26 11:25:22 -0500

    Wrote comments in each class.

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,7 @@
+/*
+
+1. Use inheritance and have a separate class for each kind of activity with a base class to contain any shared attributes or behaviors.
+2. Use polymorphism by having dervived classes override base class methods where appropriate.
+3. Follow the principles of encapsulation and abstraction by having private member variables and putting reltated items in the same class.
+
+*/
---
+++ prove/Develop05/GoalManager.cs
@@ -0,0 +1,10 @@
+/*
+
+1. Display the user's score.
+2. Allow the user to create new goals of any type.
+3. All the user to record an event where they record an event and they reveive points.
+4. Show a list of the goals. This shows wheter the goal has been completed or not:
+    [ ] compared to [X] and for the checklist goal: Completed 2/5 times.
+5. Allow the user's score and the current score to be saved and loaded.
+
+*/
