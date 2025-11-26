dc2254335d9a6ccfd894e9ff6268a3928ef935b2
rmharden <har22079@byui.edu>
2025-11-26 16:39:30 -0500

    Writing the ChecklistGoal class.

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -3,3 +3,57 @@
 Checklist Goal must be accomplished a certain number of times to be complete. Each time the user records this goal, they gain some value, but when they achieve the desired amount, they get an extra bonus.
 
 */
+
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+    public ChecklistGoal(string name, string description, int points, int amountCompleted, int target, int bonus) : base(name, description, points)
+    {
+        _amountCompleted = amountCompleted;
+        _target = target;
+        _bonus = bonus;
+    }
+    /*
+This is from the lessons from the CSE 210 7 Week course:
+
+"Each goal needs to provide a way to record the event.
+Each one needs to provide a check to see if it IS COMPLETE.
+This method should do whatever necessary for each specific kind of goal, such as making a simple goal complete and adding to the number of times a checklist goal has been completed. It should return the point value associated with recording the event. (Keep in mind that it may contain a bonus in some cases if a checklist goal was just finished.)"
+*/
+    public override void RecordEvent()
+    {
+        
+    }
+
+    /*
+        This method should return true if the goal is gompleted. The way you determine if a goal scomplete is different for each type of goal.
+    */
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    /*
+    This method should return the details of a goal that could be shown in a LIST. It should include the checkbox, the short name, and description. Then in the case of ChecklistGoal, it should be overridden in the derived class as needed.
+    */
+    public string GetDetailString()
+    {
+        if (IsComplete())
+        {
+            return $"[X] {_shortName} ({_description})";
+        }
+        else
+        {
+            return $"[ ] {_shortName} ({_description})";
+        }
+    }
+    /*
+    This method should provide all the details of a goal in a way that is easy to save a file, and then load later.
+    */
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
