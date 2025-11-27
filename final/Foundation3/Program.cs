0019e41f42fb151576f7a48da85b3dcf063e6ad2
rmharden <har22079@byui.edu>
2025-11-26 20:15:22 -0500

    Started creating Classes in the foundation 3 part of the final.

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,3 +1,18 @@
+/*
+Rebekah Harden
+CSE 210: Programming with Classes
+Brother Randall Booth
+November 26, 2025
+
+W13 Prove: Developer: Final Project
+
+Program 3: Inheritance with Event Planning
+*/
+
+
+
+
+
 using System;
 
 class Program
