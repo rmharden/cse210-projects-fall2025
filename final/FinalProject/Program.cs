105cd08f9584cb5024fdc4ee301b15323a4f67a3
rmharden <har22079@byui.edu>
2025-11-28 23:15:25 -0500

    wrote a CountCountries class with a constructor and getters and setters.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<string> countries = new List<string>();
         Person p1 = new Person("great grandmother", "Madsen", "Sidse", 1820, "Denmark", 1900, "United States of America");
         Console.WriteLine(p1.GetPersonString());
+        Console.WriteLine("Please choose the type of relative you would like to add: ");
     }
 }
