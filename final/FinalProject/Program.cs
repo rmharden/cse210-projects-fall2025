740051127806057490ce60564a85a2d4fc62fd37
rmharden <har22079@byui.edu>
2025-12-04 10:13:49 -0500

    Starting over. Was confused. Saved my code in a word document and will try again.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,44 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Genealogy Program!\nWhen finished, type 'quit'.");
-
-        EthnicityCalculator e1 = new EthnicityCalculator();
-
-        Console.Write("What is the name of the country? ");
-        string country = Console.ReadLine();
-        country = country.Trim().ToLower();
-
-        while (country != "quit")
-        {
-            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
-            e1.AddCountry(ti.ToTitleCase(country));
-
-            Console.Write("What is the name of the country? ");
-            country = Console.ReadLine();
-            country = country.Trim().ToLower();
-        }
-        e1.GetEthnicityEstimate();
+        Console.WriteLine("Hello FinalProject World!");
     }
 }
-
-// Sites I used for help:
-
-/*
-Trim()
-https://learn.microsoft.com/en-us/dotnet/api/system.string.trim?view=net-10.0
-
-ToLower()
-https://learn.microsoft.com/en-us/dotnet/api/system.string.tolower?view=net-10.0
-
-ToUpper()
-https://learn.microsoft.com/en-us/dotnet/api/system.string.toupper?view=net-10.0
-
-ToTitleCase()
-https://learn.microsoft.com/en-us/dotnet/api/system.globalization.textinfo.totitlecase?view=net-10.0
-*/
--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,3 +7,12 @@ class Program
         Console.WriteLine("Hello Foundation1 World!");
     }
 }
+
+/*
+Sites I used for help:
+
+BYU-Idaho Video by Chad Macbeth
+https://video.byui.edu/media/t/1_fnsp1qsi/259941952
+
+
+*/
