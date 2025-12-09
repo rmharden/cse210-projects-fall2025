51a55ad0471690b6ad5e120dca03ab1686625e24
rmharden <har22079@byui.edu>
2025-12-09 13:18:32 -0500

    Initialized variables in the constructor.

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -2,9 +2,10 @@ public class Comment
 {
     private string _name;
     private string _text;
-    public Comment()
+    public Comment(string name, string text)
     {
-
+        _name = name;
+        _text = text;
     }
     public void DisplayComment()
     {
