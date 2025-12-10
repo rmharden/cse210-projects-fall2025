a5593d015acdf9d005dc35a46e55147f1680401e
rmharden <har22079@byui.edu>
2025-12-09 20:06:49 -0500

    Trying to figure out what is needed as methods and member variables.

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -6,5 +6,12 @@ public class Order
     {
 
     }
-
+    public void GetOrderTotalCost()
+    {
+        // Total price is calculated as the sum of the total cost of each product plus a one-time shipping cost.
+    }
+    public string GetPackingLabel()
+    {
+        return "";
+    }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -7,3 +7,12 @@ class Program
         Console.WriteLine("Welcome to the Program 2: Encapsulation with Online Ordering Program!");
     }
 }
+
+/*
+Sites I used for help:
+
+Brother Chad Macbeth BYU-I Videos:
+https://video.byui.edu/media/t/1_fnsp1qsi
+
+
+*/
