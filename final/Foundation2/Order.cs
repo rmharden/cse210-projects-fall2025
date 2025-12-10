b84c7c7c0bc9f19d5a69acd03b8ffff1ed91d872
rmharden <har22079@byui.edu>
2025-12-09 20:12:17 -0500

    Forgot the member variables need to private. Changed that.

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,14 +1,19 @@
 public class Order
 {
     private Customer _customer;
-    public List<Product> _products;
+    private List<Product> _products;
     public Order(List<Product> products)
     {
         products = _products = new List<Product>();
     }
+    public void AddProductToOrder()
+    {
+
+    }
     public void GetOrderTotalCost()
     {
         // Total price is calculated as the sum of the total cost of each product plus a one-time shipping cost.
+        if (cust)
     }
     public string GetPackingLabel()
     {
