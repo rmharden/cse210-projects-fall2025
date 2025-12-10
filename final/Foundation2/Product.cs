c7c6e123a08442003a292c06b0e0a0944dcaecec
rmharden <har22079@byui.edu>
2025-12-09 20:56:40 -0500

    I added stubs for each class, but I'm a little stuck.

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -1,7 +1,19 @@
 public class Product
 {
-    public Product()
+    private string _productName;
+    private string _productId;
+    private double _productPrice;
+    private int _productQty;
+
+    public Product(string productName, string productId, double productPrice, int productQty)
     {
-        
+        _productName = productName;
+        _productId = productId;
+        _productPrice = productPrice;
+        _productQty = productQty;
+    }
+    public double CalculateCost()
+    {
+        return 0;
     }
 }
