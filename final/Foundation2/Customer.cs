830f484e024dad3f2addd1e786458ffdda28f34a
rmharden <har22079@byui.edu>
2025-12-09 20:22:07 -0500

    wrote stub code for Customer.

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -1,7 +1,22 @@
 public class Customer
 {
+    private Address _address;
+    private string _name;
+
     public Customer()
     {
-        
+
+    }
+    public bool IsCustomerUSA()
+    {
+        // if (_address == "USA")
+        // {
+        //     return true;
+        // }
+        // else
+        // {
+        //     return false;
+        // }
+        return true;
     }
 }
