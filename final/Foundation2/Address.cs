0c6c72397c31d39a1bb5b613413f6ff47e870622
rmharden <har22079@byui.edu>
2025-12-09 20:37:44 -0500

    I wrote the address class, but I don't know if it is correct. I'm not sure if I need getters and setters.

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -1,7 +1,42 @@
 public class Address
 {
-    public Address()
+    private string _street;
+    private string _city;
+    private string _stateProv;
+    private string _country;
+
+    public Address(string street, string city, string stateProv, string country)
     {
-        
+        _street = street;
+        _city = city;
+        _stateProv = stateProv;
+        _country = country;
+    }
+
+    // I don't know if I need the getters and the setters.
+    // public void SetCountry(string country)
+    // {
+    //     _country = country;
+    // }
+    
+    public string GetCountry()
+    {
+        return _country;
+    }
+
+    public bool IsAddressUSA()
+    {
+        if (GetCountry().ToLower() != "usa")
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public string GetAddress()
+    {
+        return $"{_street}\n{_city}, {_stateProv}, {_country}";
     }
 }
