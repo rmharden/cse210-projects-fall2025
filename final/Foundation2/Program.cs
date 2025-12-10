4257b83e683691317f027ecc33ec86cb56437d31
rmharden <har22079@byui.edu>
2025-12-10 11:21:07 -0500

    Added clearing the console and removed a word displayed to the screen.

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Program 2: Encapsulation with Online Ordering Program!");
+        Console.Clear();
+        Console.WriteLine("\nWelcome to Program 2: Encapsulation with Online Ordering!\n");
     }
 }
 
