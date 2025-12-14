e369a3aa1cd69bef830372e4ea67edbb9302a7d9
rmharden <har22079@byui.edu>
2025-12-14 00:37:02 -0500

    wrote methods that are abstract and working on the GetSummary method in the base class.

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,12 +1,22 @@
+using System.Runtime.InteropServices;
+
 public abstract class Activity
 {
-    private string _date;
-    private string _name;
-    private double _minutes;
+    protected string _date;
+    protected string _name;
+    protected double _minutes;
 
     public Activity(string date, string name, double minutes)
     {
         _date = date;
-        
+        _name = name;
+        _minutes = minutes;
+    }
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+    public virtual string GetSummary()
+    {
+        return $"{_date}" {_name};
     }
 }
