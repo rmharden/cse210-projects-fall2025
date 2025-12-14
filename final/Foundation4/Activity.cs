d985896a73cc9384fc4ff8c74511749c02338323
rmharden <har22079@byui.edu>
2025-12-14 00:32:09 -0500

    Trying to write the Exercise program. Writing the member variables and methods.

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,12 @@
+public abstract class Activity
+{
+    private string _date;
+    private string _name;
+    private double _minutes;
+
+    public Activity(string date, string name, double minutes)
+    {
+        _date = date;
+        
+    }
+}
