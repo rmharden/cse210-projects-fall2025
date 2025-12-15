34ec8fbab322e5ed070b88f7b96efb6ae0c7d8e8
rmharden <har22079@byui.edu>
2025-12-15 09:47:00 -0500

    Wrote an example so I can see what it should look like in the console.

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -15,5 +15,16 @@ class Program
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Foundation3 World!");
+
+        // 1st example:
+        Console.WriteLine($"\nExample 1:");
+
+        Console.WriteLine($"\nObject Oriented Programming - Inheritance\nJanuary 1, 2023 @ 9:00am\n123 Elm St\nNew York City, NY, USA\n");
+
+        Console.WriteLine($"Type: Lecture\nObject Oriented Programming - Inheritance\nJanuary 1, 2023 @ 9:00am\n123 Elm St\nNew York City, NY, USA\nSpeaker: Bob the Builder\nCapacity: 100\nLecuture - Object Oriented Programming - January 1, 2023\n========================================");
+
+        // 2nd example:
+
+
     }
 }
