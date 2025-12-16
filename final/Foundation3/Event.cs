59294b5fabf1b7f8480acdc7bb5f459ad8eff9d2
rmharden <har22079@byui.edu>
2025-12-15 19:13:30 -0500

    Going through instructions to know what each method in the base class needs.

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -24,5 +24,35 @@ public abstract class Event
         Console.WriteLine(StandardDetails());
         Console.WriteLine($"");
     }
-    
+    public string GetShortDescription()
+    {
+        return _eventTitle + _description + _date + _address;
+    }
 }
+
+
+/*        
+        Console.WriteLine($"\nObject Oriented Programming - Inheritance\nJanuary 1, 2023 @ 9:00am\n123 Elm St\nNew York City, NY, USA\n");
+
+        Console.WriteLine($"Type: Lecture\nObject Oriented Programming - Inheritance\nJanuary 1, 2023 @ 9:00am\n123 Elm St\nNew York City, NY, USA\nSpeaker: Bob the Builder\nCapacity: 100\n");
+
+        Console.WriteLine($"Lecuture - Object Oriented Programming - January 1, 2023\n========================================");
+
+        // 2nd example:
+        Console.WriteLine($"\nExample 2:");
+
+        Console.WriteLine($"Graduation - MSD 321 Graduation Party\nJune 1, 2023\n123 Elm St\nNew York City, NY, USA\n");
+
+        Console.WriteLine($"\nType: Reception\nGraduation - MSD 321 Graduation Party\nJune 1, 2023 @ 7:00pm\n123 Elm St\nNew York City, NY, USA\nEmail: grad@msd321.com\n");
+
+        Console.WriteLine($"\nReception - Graduation - June 1. 2023\n========================================");
+
+        // 3rd example:
+        Console.WriteLine($"\nExample 3:");
+
+        Console.WriteLine($"\nBridge Tour - Tour the London Bridge\nOctober 13,2023 @ 11:30pm\n246 Oak Circle\nLondon, England, UK\n");
+
+        Console.WriteLine($"Type: Outdoor Gathering\nBridge Tour - Tour the London Bridge\nOctober 13, 2023 @ 11:30am\n246 Oak Circle\nLondon, England, UK\nWeather: Chance of Rain and Wind\n");
+
+        Console.WriteLine($"\nOutdoor Gathering: Bridge Tour - October 13, 2023\n========================================");
+        */
